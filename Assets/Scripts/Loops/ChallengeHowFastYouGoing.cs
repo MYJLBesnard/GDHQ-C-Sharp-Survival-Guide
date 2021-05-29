f7b30d62b74a8a127b7ef6c586b0a1ea5be5fec9
Michel YJL Besnard <81327793+MYJLBesnard@users.noreply.github.com>
2021-05-29 20:32:51 -0300

    Loop challenges completed but not yet reviewed

--- Assets/Scripts/Loops/ChallengeHowFastYouGoing.cs
+++ Assets/Scripts/Loops/ChallengeHowFastYouGoing.cs
@@ -8,15 +8,30 @@ public class ChallengeHowFastYouGoing : MonoBehaviour
     // When speed is greater than MaxSpeed, stop incrementing (random number between
     // 60 and 120).  Hint: use a couroutine.
 
+    public int maxSpeed;
+    public int speed;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        maxSpeed = Random.Range(60, 120);
+        StartCoroutine(MaxSpeedReached());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      
     }
+
+    IEnumerator MaxSpeedReached()
+    {
+        while (speed != maxSpeed)
+        {
+            speed++;
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+
 }
--- Assets/Scripts/Loops/ChallengeUltimatePrintOuts.cs
+++ Assets/Scripts/Loops/ChallengeUltimatePrintOuts.cs
@@ -6,12 +6,32 @@ public class ChallengeUltimatePrintOuts : MonoBehaviour
 {
     // Create a program that prints out 0-10, then prints out even numbers from 11-20,
     // and only odd numbers from 21-30.  Hint use modulus operator.
-
+    public int myNumber;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < 30; i++)
+        {
+            if (i <= 10)
+            {
+                Debug.Log(i);
+            }
+            else if (i >= 10 && i <= 20)
+            {
+                if (i % 2 == 0)
+                {
+                    Debug.Log(i);
+                }
+            }
+            else if (i > 20)
+            {
+                if (i % 2 == 1)
+                {
+                    Debug.Log(i);
+                }
+            }
+        }
     }
 
     // Update is called once per frame
