e1d8c6b8232a514d33e1fdb0a4684aca188eb765
Michel YJL Besnard <81327793+MYJLBesnard@users.noreply.github.com>
2021-05-29 08:15:58 -0300

    Added 3 folders and all associated blank scripts

--- Assets/Scripts/Switch Statements/ChallengeWeaponSelectProgram.cs
+++ Assets/Scripts/Switch Statements/ChallengeWeaponSelectProgram.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestVariable : MonoBehaviour
+public class ChallengeWeaponSelectProgram : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/Variables/ChallengeTipCalculator.cs
+++ Assets/Scripts/Variables/ChallengeTipCalculator.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestIFStatement : MonoBehaviour
+public class ChallengeTipCalculator : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
