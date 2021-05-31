99afe93e653fe6e142f3296c6aa68d3e51a26aa0
MYJLBesnard <myjl.besnard@gmaal.com>
2021-05-30 21:07:04 -0300

    Update ChallengeWasThatAMagicTrick.cs

--- Assets/Scripts/Coroutines (IEnumerator)/ChallengeWasThatAMagicTrick.cs
+++ Assets/Scripts/Coroutines (IEnumerator)/ChallengeWasThatAMagicTrick.cs
@@ -13,6 +13,25 @@ public class ChallengeWasThatAMagicTrick : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+<<<<<<< Updated upstream
         
+=======
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            Debug.Log("The 'i' key has been pressed.");
+            GetComponent<MeshRenderer>().enabled = false;
+            Debug.Log("The cube has vanished!");
+
+            StartCoroutine(HideTheCube());
+        }
+    }
+
+    IEnumerator HideTheCube()
+    {
+
+        yield return new WaitForSeconds(5.0f);
+        Debug.Log("The wait is over...");
+        GetComponent<MeshRenderer>().enabled = true;
+>>>>>>> Stashed changes
     }
 }
